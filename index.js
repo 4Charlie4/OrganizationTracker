a84757331faabd85f1daf4b8d128d03ad6ab9b19
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-09-01 18:29:07 -0400

    initial prompt

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const db = require("./db/connection");
 const cTable = require("console.table");
-const inquirer = require("inquirer");
-const mysql = require("mysql");
+const Organization = require("./utils/index");
 
 db.connect((err) => {
-    if (err) throw err;
-    console.log("database connected")
-    
-})
+  if (err) throw err;
+  console.log("database connected");
+
+  const database = new Organization();
+
+  database.init();
+});
--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,31 @@
-const mysql = require("mysql");
+
 const inquirer = require("inquirer");
 
-class organization {
-    constructor()
+class Organization {
+  constructor() {}
+
+  init() {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "userChoice",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View all Departments",
+          "Add Department",
+          "Exit",
+        ],
+      },
+    ]);
+  }
+  viewAllEmployees() {
+
+  }
 }
 
+module.exports = Organization;
